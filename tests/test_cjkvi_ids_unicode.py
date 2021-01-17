d9f591bdac2620bb21a11d62ed1b36e6d31a47b3
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-17 20:34:32 +0800

    Add several unit tests for GlyphWiki and GH action

--- tests/test_cjkvi_ids_unicode.py
+++ tests/test_cjkvi_ids_unicode.py
@@ -1,5 +1,7 @@
 from cjkvi_ids_unicode import __version__
 import cjkvi_ids_unicode.utils as utils
+import cjkvi_ids_unicode.driver as driver
+import cjkvi_ids_unicode.constants as constants
 
 
 def test_version():
@@ -15,3 +17,14 @@ def test_ucs_to_glyphwiki_key():
     assert utils.convert_ucs_to_glyphwiki_key("U-v002+4E11") == "u4e11-var-002"
     assert utils.convert_ucs_to_glyphwiki_key("U-i001+864D") == "u864d-itaiji-001"
     assert utils.convert_ucs_to_glyphwiki_key("U-i001+2D94D") == "u2d94d-itaiji-001"
+
+
+def test_glyphwiki_resolution():
+    glyphwiki = driver.GlyphWiki(constants.GLYPHWIKI_VARIANT_INFO)
+    assert glyphwiki.resolve("aj1-02177") == "山"
+    assert glyphwiki.resolve("aj1-03708") == "卜"
+    assert glyphwiki.resolve("cdp-8cc6") == "亡"
+    assert glyphwiki.resolve("cdp-86d6-itaiji-001") == "黽"
+    assert glyphwiki.resolve("u2d94d-itaiji-001") == "𭥍"
+    assert glyphwiki.resolve("gt-k00880") == "虍"
+    assert glyphwiki.resolve("gt-k01770") == "𤰔"
